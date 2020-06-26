6ef5f904d756ba11901a788fc87f4c3ac718deb1
Shivam093 <shuklashivam1971@gmail.com>
2020-06-27 00:23:04 +0530

    Updates

---
+++ precache-manifest.1d3413cb6fddc9aa8060eadf1012fe26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff1910d955cc634699155e8574b8696",
+    "url": "/Face-Recognition/index.html"
+  },
+  {
+    "revision": "e529af919851940be119",
+    "url": "/Face-Recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c1abe5c1d18ac94a778c",
+    "url": "/Face-Recognition/static/css/main.1d6adaf3.chunk.css"
+  },
+  {
+    "revision": "e529af919851940be119",
+    "url": "/Face-Recognition/static/js/2.06dc8c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Recognition/static/js/2.06dc8c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1abe5c1d18ac94a778c",
+    "url": "/Face-Recognition/static/js/main.bfa9218e.chunk.js"
+  },
+  {
+    "revision": "da2e3b9f15aec6704088",
+    "url": "/Face-Recognition/static/js/runtime-main.3e2ad280.js"
+  }
+]);
